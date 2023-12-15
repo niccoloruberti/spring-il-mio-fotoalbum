ae0d820b4ec013d3faeac8a54168cf05541ab499
niccoloruberti <nikiruberti@gmail.com>
2023-12-15 13:00:15 +0100

    index con foto

--- src/main/java/org/java/spring/controller/PhotoController.java
+++ src/main/java/org/java/spring/controller/PhotoController.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.java.spring.db.pojo.Photo;
 import org.java.spring.db.serv.PhotoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class PhotoController {
 
 	@Autowired
