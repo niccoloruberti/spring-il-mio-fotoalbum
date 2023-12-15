c64ee51687cc3c971a28790ef309ab3202d3e825
niccoloruberti <nikiruberti@gmail.com>
2023-12-15 14:50:36 +0100

    show della singola foto

--- src/main/java/org/java/spring/controller/PhotoController.java
+++ src/main/java/org/java/spring/controller/PhotoController.java
@@ -2,12 +2,14 @@ package org.java.spring.controller;
 
 import java.util.List;
 
+import org.java.spring.db.pojo.Category;
 import org.java.spring.db.pojo.Photo;
 import org.java.spring.db.serv.PhotoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class PhotoController {
@@ -23,5 +25,19 @@ public class PhotoController {
 		model.addAttribute("photos", photos);
 		
 		return "photos";
+	}
+	
+	@GetMapping("/photos/{id}")
+	public String getPhoto(Model model, @PathVariable int id) {
+		
+		Photo photo = photoService.findById(id);
+		
+		List<Category> categories = photo.getCategories();
+		
+		model.addAttribute("photo", photo);
+		
+		model.addAttribute("categories", categories);
+		
+		return "photo";
 	}
 }
